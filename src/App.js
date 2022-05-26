09b221238ba826f2d868e3bab010b74e73149fe1
Rashmi Mhatre <rasmhatr@WKMZTDFA4678.global.publicisgroupe.net>
2022-05-26 12:43:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import WebsitesByTraffic from "./components/WebsitesByTraffic";
+
+function App() {
+  return (
+    <div className="App">
+      <WebsitesByTraffic />
+    </div>
+  );
+}
+
+export default App;
